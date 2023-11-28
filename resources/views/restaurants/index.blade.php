c3143638b845df24c63c79611d8e772943ed37dd
raimeiga <yusuke612@hotmail.com>
2023-11-28 19:39:31 +0900

    ５.店舗一覧のページ作成

---
+++ resources/views/restaurants/index.blade.php
@@ -0,0 +1,25 @@
+<!-- 店舗一覧のページ -->
+
+<a href="{{ route('restaurants.create') }}"> Create New Restaurant</a>
+ 
+ <table>
+     <tr>
+         <th>Name</th>
+         <th>Description</th>
+         <th>Price</th>
+         <th>Category ID</th>
+         <th >Action</th>
+     </tr>
+     @foreach ($restaurants as $restaurant)
+     <tr>
+         <td>{{ $restaurant->name }}</td>
+         <td>{{ $restaurant->description }}</td>
+         <td>{{ $restaurant->price }}</td>
+         <td>{{ $restaurant->category_id }}</td>  <!--　こここれでいいのか？店とカテゴリは中間テーブルで紐づいている -->
+         <td>
+             <a href="{{ route('restaurants.show',$restaurant->id) }}">Show</a>
+             <a href="{{ route('restaurants.edit',$restaurant->id) }}">Edit</a>
+         </td>
+     </tr>
+     @endforeach
+ </table>
