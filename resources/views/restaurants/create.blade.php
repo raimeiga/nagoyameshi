19ba0607283e9e5fb2a041680b0d4ca107498c70
raimeiga <yusuke612@hotmail.com>
2023-11-29 13:41:52 +0900

    店舗の新規登録画面の作成・編集

---
+++ resources/views/restaurants/create.blade.php
@@ -0,0 +1,60 @@
+<!-- 店舗の新規登録画面 管理者が店舗を登録するってことだよな？-->
+<div>
+     <h2>Add New Restaurant</h2>
+ </div>
+ <div>
+     <a href="{{ route('restaurants.index') }}"> Back</a>
+ </div>
+ 
+ <form action="{{ route('restaurants.store') }}" method="POST">
+     @csrf
+ 
+     <div>
+         <strong>Name:</strong>
+         <input type="text" name="name" placeholder="Name">
+     </div>
+
+     <div>
+         <strong>Category:</strong>
+         <select name="category_id">
+         @foreach ($categories as $category)
+         <option value="{{ $category->id }}">{{ $category->name }}</option>
+         @endforeach
+         </select>
+     </div>
+     
+     <div>
+         <strong>Price:</strong>
+         <input type="text" name="price" placeholder="Price">
+     </div>
+     
+     <div>
+         <strong>Hours:</strong>
+         <input type="text" name="hours" placeholder="Hours">
+     </div>
+
+     <div>
+         <strong>Holiday:</strong>
+         <input type="text" name="holiday" placeholder="Holiday">
+     </div>
+
+     <div>
+         <strong>Description:</strong>
+         <textarea style="height:150px" name="description" placeholder="Description"></textarea>
+     </div>
+
+     <div>
+         <strong>Address:</strong>
+         <input type="text" name="address" placeholder="Address">
+     </div>
+     
+     <div>
+         <strong>Phone:</strong>
+         <input type="tel" name="phone" placeholder="Phone">
+     </div>
+     
+     <div>
+         <button type="submit">Submit</button>
+     </div>
+ 
+ </form>
