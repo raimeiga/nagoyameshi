ce551e1d27f50882e4ed151311f02d3343167d69
raimeiga <yusuke612@hotmail.com>
2023-11-29 13:40:37 +0900

    改行しただけ

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -5,20 +5,29 @@
  <table>
      <tr>
          <th>Name</th>
+
          <th>Description</th>
+
          <th>Price</th>
+
          <th>Category ID</th>
-         <th >Action</th>
+         
+         <th>Action</th>
      </tr>
      @foreach ($restaurants as $restaurant)
      <tr>
          <td>{{ $restaurant->name }}</td>
-         <td>{{ $restaurant->description }}</td>
-         <td>{{ $restaurant->price }}</td>
          <td>{{ $restaurant->category_id }}</td>  <!--　こここれでいいのか？店とカテゴリは中間テーブルで紐づいている -->
+         <td>{{ $restaurant->price }}</td>
+         <td>{{ $restaurant->hours }}</td>
          <td>
+             <form action="{{ route('restaurants.destroy',$restaurant->id) }}" method="POST">
              <a href="{{ route('restaurants.show',$restaurant->id) }}">Show</a>
              <a href="{{ route('restaurants.edit',$restaurant->id) }}">Edit</a>
+             @csrf
+                 @method('DELETE')
+                 <button type="submit">Delete</button>
+         </form>
          </td>
      </tr>
      @endforeach
