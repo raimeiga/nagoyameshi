fa591a126ed2bcbca2be5b383900e1961f9eb74c
raimeiga <yusuke612@hotmail.com>
2023-12-08 17:07:45 +0900

    Lunch_lowest_priceテーブル作成・データ挿入のための編集

---
+++ database/migrations/2023_12_08_164754_create_lunch_lowest_prices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lunch_lowest_prices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('price')->unsigned(); //各店舗の昼の最低価格
+            // ↓ 親モデルの小文字クラス名に「_id」という接尾辞を付けることで、Restaurantモデルに対する外部キーカラムを設定 
+            $table->integer('restaurant_id')->unsigned(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lunch_lowest_prices');
+    }
+};
