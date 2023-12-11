ac511f5515da324abafe3b4d672a2bbdf312f788
raimeiga <yusuke612@hotmail.com>
2023-12-11 17:06:01 +0900

    lunch_lowest_priceテーブルがないので必要なしになり、紐づけコードを削除

--- app/Models/Lunch_lowest_price.php
+++ app/Models/Lunch_lowest_price.php
@@ -9,7 +9,5 @@ class Lunch_lowest_price extends Model
 {
     use HasFactory;
 
-    public function restaurants() {
-        return $this->hasMany(Restaurant::class)->withTimestamps();
-    }
+    
 }
