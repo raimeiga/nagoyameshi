63f38a008f84c8fa1a803083cb0ce1698657bca9
raimeiga <yusuke612@hotmail.com>
2023-12-12 21:10:47 +0900

    8章 CSSフレームワークを使おう/8.2 Bootstrapでこれまでの実装を修正/店舗の新規登録画面

--- resources/views/restaurants/create.blade.php
+++ resources/views/restaurants/create.blade.php
@@ -1,70 +1,61 @@
-<!-- 店舗の新規登録画面 管理者が店舗を登録するってことだよな？-->
-<div>
-     <h2>Add New Restaurant</h2>
- </div>
- <div>
-     <a href="{{ route('restaurants.index') }}"> Back</a>
- </div>
+@extends('layouts.app')
  
- <form action="{{ route('restaurants.store') }}" method="POST">
-     @csrf
+ @section('content')
+ <div class="container">
+     <h1>新しい店舗を追加</h1>
  
-     <div>  
-         <strong>Name:</strong>
-         <input type="text" name="name" placeholder="Name">
-     </div>
+     <form action="{{ route('restaurants.store') }}" method="POST">
+         @csrf
+         <div class="form-group">
+             <label for="product-name">店名</label>
+             <input type="text" name="name" id="restaurant-name" class="form-control">
+         </div>       
 
-     <div>
-        <strong>Category:</strong>
+        <div class="form-group">
+        <label for="restaurant-category">カテゴリ</label><br>
             <!--↓ $categoriesは、RestaurantControllerのcreateアクションから渡されたCategoryモデルのインスタンス（categoriesテーブルの全カラム） -->
             @foreach ($categories as $category) 
             <input type="checkbox" name="category_ids[]" value="{{ $category->id }}" >{{ $category->name }}
             @endforeach
-                    
-            <!-- 店とカテゴリは「多対多」。チェックボックスに複数チェックが入ると、value属性の値{{ $category->id }}にチェックしたカテゴリのidが格納される。
-                 このように、複数のvalue属性の値を受け取るには、name属性の値に[]（角括弧）をつけて配列にする。
-                 つまりname属性の値をcategory_ids[]にして,value属性の値を$category->id にする。
-                 あとは、コントローラのstoreアクションに$request->input('category_ids')を記述することで、チェックしたカテゴリの
- 　              IDの配列が取得できる。 -->
-     </div>
-     <div>       
-         <strong>price:</strong>
-         <input type="text" name="price" placeholder="price">
-     </div> 
-     
-     <div>
-         <strong>Hours:</strong>
-         <input type="text" name="hours" placeholder="Hours">
-     </div>
+        </div>
 
-     <div>
-         <strong>Holiday:</strong>
-         <input type="checkbox" name="holiday" value="無休" >無休
-         <input type="checkbox" name="holiday" value="日曜日" >日曜日
-         <input type="checkbox" name="holiday" value="月曜日" >月曜日
-         <input type="checkbox" name="holiday" value="火曜日" >火曜日
-         <input type="checkbox" name="holiday" value="水曜日" >水曜日
-         <input type="checkbox" name="holiday" value="木曜日" >木曜日
-         <input type="checkbox" name="holiday" value="金曜日" >金曜日
-         <input type="checkbox" name="holiday" value="土曜日" >土曜日         
-     </div>
+         <div class="form-group">
+             <label for="restaurant-price">予算</label>
+             <input type="number" name="price" id="restaurant-price" class="form-control">
+         </div>
+         
+         <div class="form-group">
+             <label for="restaurant-hours">営業時間</label>
+             <input type="text" name="hours" id="restaurant-hours" class="form-control">
+         </div>
 
-     <div>
-         <strong>Description:</strong>
-         <textarea style="height:150px" name="description" placeholder="Description"></textarea>
-     </div>
+         <div class="form-group">
+             <label for="restaurant-holiday">定休日</label>
+             <input type="text" name="holiday" id="restaurant-holiday" class="form-control">
+         </div>
 
-     <div>
-         <strong>Address:</strong>
-         <input type="text" name="address" placeholder="Address">
-     </div>
-     
-     <div>
-         <strong>Phone:</strong>
-         <input type="tel" name="phone" placeholder="Phone">
-     </div>
-     <div>
-         <button type="submit">Submit</button>
-     </div>
+         <div class="form-group">
+             <label for="restaurant-description">説明</label>
+             <textarea name="description" id="restaurant-description" class="form-control"></textarea>
+         </div>
+        
+         <div class="form-group">
+             <label for="restaurant-address">住所</label>
+             <input type="text" name="address" id="restaurant-address" class="form-control">
+         </div>
+
+         <div class="form-group">
+             <label for="restaurant-phone">電話番号</label>
+             <input type="tel" name="phone"" id="restaurant-phone"" class="form-control">
+         </div>
+
+
+         
+
+         <button type="submit" class="btn btn-success">商品を登録</button>
+
+     </form>
  
- </form>
+     <a href="{{ route('restaurants.index') }}">商品一覧に戻る</a>
+ </div>
+ @endsection
