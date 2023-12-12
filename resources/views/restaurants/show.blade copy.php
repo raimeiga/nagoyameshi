47e0f7e03a91c966a84d45a446cbee8b583dac8d
raimeiga <yusuke612@hotmail.com>
2023-12-12 23:31:38 +0900

    Bootstrapで修正する前のコードを残しておく

---
+++ resources/views/restaurants/show.blade copy.php
@@ -0,0 +1,53 @@
+<!-- 店舗詳細画面 -->
+<div>
+     <h2> Show Restaurant</h2>
+ </div>
+ <div>
+     <a href="{{ route('restaurants.index') }}"> Back</a>
+ </div>
+ 
+ <div>
+     <strong>Name:</strong>
+     {{$restaurant->name}}
+ </div>
+ 
+ <div>
+     <strong>Category:</strong>
+     @foreach ($restaurant->categories as $category)
+                {{$category->name}}
+     @endforeach 
+ </div>
+
+ <div>
+     <strong>Price:</strong>
+     {{$restaurant->price}} 
+ </div>
+
+ <div>
+     <strong>Hours:</strong>
+     {{$restaurant->hours}}
+ </div>
+
+ <div>
+     <strong>Holiday:</strong>
+     {{$restaurant->holiday}}
+ </div>
+
+ <div>
+     <strong>Description:</strong>
+     {{$restaurant->description}}
+ </div>
+ <div>
+     <strong>Address:</strong>
+     {{$restaurant->address}}
+ </div>
+ <div>
+     <strong>Phone:</strong>
+     {{$restaurant->phone}}
+ </div>
+
+<td>
+  <a href="{{ route('restaurants.edit',$restaurant->id) }}">Edit</a>
+</td>
+ 
+ 
