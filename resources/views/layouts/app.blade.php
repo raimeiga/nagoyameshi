872ebb6a4040369a934f73ecbf2da4ffcec5a8e4
raimeiga <yusuke612@hotmail.com>
2023-12-13 01:17:06 +0900

    8章 CSSフレームワークを使おう/8.3 Font Awesomeを導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    
+    <script src="https://kit.fontawesome.com/0d8153b09a.js" crossorigin="anonymous"></script>
      <!-- Styles -->
      <link href="{{ asset('css/samuraimart.css') }}" rel="stylesheet">
 </head>
