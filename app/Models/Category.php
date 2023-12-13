7eca7e4b29e77f5ef5e1f6bc56b7d055dca9d063
raimeiga <yusuke612@hotmail.com>
2023-12-13 13:48:55 +0900

    小文字から大文字に修正［belongsToMany(restaurant::class)を、belongsToMany(Restaurant::class)］へ修正.これまでの大丈夫か?!

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,6 @@ class Category extends Model
     use HasFactory;
 
     public function restaurants() {
-        return $this->belongsToMany(restaurant::class)->withTimestamps();
+        return $this->belongsToMany(Restaurant::class)->withTimestamps();
     }   
 }
