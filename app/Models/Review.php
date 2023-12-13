eadd11b54bac5dc702cc229db7abd4de2277873a
raimeiga <yusuke612@hotmail.com>
2023-12-13 13:49:05 +0900

    ユーザーとレビューの紐づけ

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -13,4 +13,9 @@ class Review extends Model
     {
         return $this->belongsTo(Restaurant::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
