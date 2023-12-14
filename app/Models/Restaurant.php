3208c28283af8ab1a8172985e5330270ec360e70
raimeiga <yusuke612@hotmail.com>
2023-12-14 09:40:21 +0900

    15章 ソート機能を追加/15.2 ソート機能を追加/モデルを修正

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 //お気に入りされる（今回のケースでは店舗Restaurant）モデルにuse Favoriteableとすることで、お気に入り機能を使える
 use Overtrue\LaravelFavorite\Traits\Favoriteable;  
+// ↓ ソート機能を使えるようになる宣言
+use Kyslik\ColumnSortable\Sortable;
+
 class Restaurant extends Model
 {
-    use HasFactory, Favoriteable;
+    use HasFactory, Favoriteable, Sortable;
 
     public function categories() {
         return $this->belongsToMany(category::class)->withTimestamps();
