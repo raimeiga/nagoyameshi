a98d6ac53fe390802d9a50135216c9029a3aabb3
raimeiga <yusuke612@hotmail.com>
2023-12-16 02:36:31 +0900

    お気に入り店舗一覧ページのビューを作成

---
+++ resources/views/users/favorite.blade.php
@@ -0,0 +1,42 @@
+<!-- お気に入り店舗一覧 -->
+@extends('layouts.app')
+ 
+ @section('content')
+ <div class="container  d-flex justify-content-center mt-3">
+     <div class="w-75">
+         <h1>お気に入り店舗一覧</h1>
+ 
+         <hr>
+ 
+         <div class="row">
+             @foreach ($favorites as $fav)
+             <div class="col-md-7 mt-2">
+                 <div class="d-inline-flex">
+                     <a href="{{route('restaurants.show', $fav->favoriteable_id)}}" class="w-25">
+                         <img src="{{ asset('img/dummy.png')}}" class="img-fluid w-100">
+                     </a>
+                     <div class="container mt-3">
+                         <h5 class="w-100 samuraimart-favorite-item-text">店舗名：{{App\Models\Restaurant::find($fav->favoriteable_id)->name}}</h5>                    
+                         <!-- ↓　ココにお気に入り登録された店舗のカテゴリを並べたいけど、書き方がわからない。↓で書くと何も表示されない。 -->
+                         <h5 class="w-100 samuraimart-favorite-item-text">カテゴリ：{{App\Models\Restaurant::find($fav->favoriteable_id)->category_id}}</h5> 
+                         <!-- ↓も、お気に入り登録された店舗のカテゴリが表示されているのでなく、ただ、カテゴリ名が繰り返されているだけ。 -->
+                         <!-- <h5 class="w-100 samuraimart-favorite-item-text">カテゴリ：{{App\Models\Category::find($fav->favoriteable_id)->name}}</h5> -->
+
+                         <h6 class="w-100 samuraimart-favorite-item-text">予算：&yen;{{App\Models\Restaurant::find($fav->favoriteable_id)->price}}</h6>
+                         <h6 class="w-100 samuraimart-favorite-item-text">営業時間：{{App\Models\Restaurant::find($fav->favoriteable_id)->hours}}</h6>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-md-2 d-flex align-items-center justify-content-end">
+                 <a href="{{ route('restaurants.favorite', $fav->favoriteable_id) }}" class="samuraimart-favorite-item-delete">
+                     削除
+                 </a>
+             </div>
+             
+             @endforeach
+         </div>
+ 
+         <hr>
+     </div>
+ </div>
+ @endsection
