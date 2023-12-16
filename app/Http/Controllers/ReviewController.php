9d440cf368acbafd14e5a20c5503b4f6e8c1ef61
raimeiga <yusuke612@hotmail.com>
2023-12-16 11:57:47 +0900

    レビューの投稿機能（storeアクション）に、評価数（★数）を追加し、評価数もreviewsテーブルのカラムに保存されるようにした。

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -25,6 +25,7 @@ class ReviewController extends Controller
         $review->content = $request->input('content');
         $review->restaurant_id = $request->input('restaurant_id');
         $review->user_id = Auth::user()->id;
+        $review->score = $request->input('score');
         $review->save();
 
         return back();
