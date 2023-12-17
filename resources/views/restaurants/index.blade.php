cf959c572369cd6e0d22428110756fcccd21fbc8
raimeiga <yusuke612@hotmail.com>
2023-12-17 21:57:48 +0900

    ライブラリの店舗管理画面の作成/画像追加の設定に伴い、ブラウザの店舗一覧画面に画像追加できるよう編集

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -27,7 +27,11 @@
                  @foreach($restaurants as $restaurant)
                  <div class="col-3">
                      <a href="{{route('restaurants.show', $restaurant)}}">
+                     @if ($restaurant->image !== "")
+                         <img src="{{ asset($restaurant->image) }}" class="img-thumbnail">
+                     @else
                          <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                     @endif
                      </a>
                      <div class="row">
                          <div class="col-12">
