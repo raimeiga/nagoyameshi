ceb9d95557aeeac33c7e177f41cfa5fa0b003678
raimeiga <yusuke612@hotmail.com>
2023-12-20 05:07:25 +0900

    余計な表記を削除

--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -10,7 +10,7 @@
      <div class="col-9">
         <div class="container">   
              @if ($category !== null)
-                 <a href="{{ route('restaurants.index') }}">トップ</a> > <a href="#">{{ $category->name }}</a> > {{ $category->name }}
+                 <a href="{{ route('restaurants.index') }}">トップ</a> > <a href="#">{{ $category->name }}</a> >
                  <h1>{{ $category->name }}の店舗一覧{{$total_count}}件</h1>
              @endif        
         </div>
